e6b26783de82d6f8fdabbbefc403c2ca96079ab5
ordunamonse <ordunamonse28@gmail.com>
2025-06-11 17:09:20 -0600

    proyecto

--- billingApp_v2/dbfiles/init-user-db.sh
+++ billingApp_v2/dbfiles/init-user-db.sh
@@ -7,3 +7,14 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
     GRANT ALL PRIVILEGES ON DATABASE billingapp_db TO billingapp;
     GRANT ALL ON SCHEMA public TO billingapp;
 EOSQL
+
+#!/bin/bash
+set -e
+mysql -u root -pqwerty sistema_ventas_db <<-EOSQL
+    CREATE TABLE IF NOT EXISTS productos (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        nombre VARCHAR(100) NOT NULL,
+        precio DECIMAL(10,2) NOT NULL
+    );
+    INSERT INTO productos (nombre, precio) VALUES ('Producto 1', 19.99);
+EOSQL
